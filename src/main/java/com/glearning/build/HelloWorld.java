c701f71b130d8584029e8b8ca60772a443f1d4ce
pradeep <pradeep.kumar44@gmail.com>
2020-02-08 12:44:34 +0530

    committing the test cases

--- src/main/java/com/glearning/build/HelloWorld.java
+++ src/main/java/com/glearning/build/HelloWorld.java
@@ -5,4 +5,21 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
     }
+
+    public int sum(int a , int b){
+        return a + b;
+    }
+
+    public int sub(int a , int b){
+        return a - b;
+    }
+
+    public int product(int a , int b){
+        return a * b;
+    }
+
+    public int divide(int a , int b){
+        return a / b;
+    }
+
 }
---
+++ src/test/java/com/glearning/build/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.glearning.build;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+ @Test
+ public void testSum(){
+     HelloWorld helloWorld = new HelloWorld();
+     int result = helloWorld.sum(45, 56);
+     Assert.assertEquals(10, result);
+ }
+
+}
